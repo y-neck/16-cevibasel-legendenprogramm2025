a2f6f68e752fef18e52d69b46857f41735963719
Yannick Spriessler <87867143+y-neck@users.noreply.github.com>
2025-01-30 17:22:31 +0100

    admin ui

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,10 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.send('admin login page')
+    res.render('admin')
 })
 router.get('/backend', (req, res) => {
-    res.send('Admin page')
+    res.render('admin/backend')
 })
 
 module.exports = router;
